896ad819b66dc47908e5d501163bbdc484530fed
Danny Navejas <dannynavejas@gmail.com>
2019-05-07 21:55:32 -0500

    order taker and cook currently animate with no relation to eachother

--- pizza-kitchen.js
+++ pizza-kitchen.js
@@ -1,29 +1,67 @@
-"use strict"
-let orderTaker = document.getElementById("order-taker");
+'use strict'
+let orderTaker = document.querySelector("#order-taker");
+let pause = document.getElementById("pause");
 let cook = document.getElementById("cook");
+let fridge = document.getElementById("freezer");
 
-function giveOrder(e){
-    switch(e.type){
-        case "animationstart":
-        console.log("start");
-        break;
-
-        case "animationend":
-        console.log("end");
-        break;
-
-        case "animationiteration":
-        if (orderTaker.attributes["src"].nodeValue.indexof('right')>0){
-            orderTaker.attributes["src"].nodeValue = "pizza-man-left.gif";
-        }
-        else{
-            orderTaker.attributes["src"].nodeValue = "pizza-man-left.gif";
-        }
-        console.log(`new loop after ${e.elapsedTime} seconds`);
-        break;
-    }
+// console.log(e)
+function toggleChef(e) {
+    orderTaker.classList.toggle('paused');
+    cook.classList.toggle('paused');
 }
-window.onload = giveOrder;
-orderTaker.addEventListener("animationstart", listener, false);
-orderTaker.addEventListener("animationend", listener, false);
-orderTaker.addEventListener("animationiteration", listener, false);
+
+function grabPizza(e) {
+	switch(e.type) {
+		case "animationstart":
+		console.log(e)
+			break;
+			
+
+		case "animationend":
+		console.log(e)
+		  break;
+
+		case "animationiteration":
+		console.log(e)
+			if(cook.attributes["src"].nodeValue.indexOf('right') > 0) {
+                console.log("test");
+                cook.attributes['src'].nodeValue = "pizza-man-left.gif";
+                fridge.attributes['src'].nodeValue = "freezer-open.png";
+                
+			}
+			else {
+				cook.attributes['src'].nodeValue = "pizza-man-right.gif";
+			}
+		  break;
+	}
+}
+
+function takeOrder(e){
+    switch(e.type) {
+		case "animationstart":
+		  break;
+
+		case "animationend":
+		  break;
+
+		case "animationiteration":
+			if(orderTaker.attributes["src"].nodeValue.indexOf('right') > 0) {
+				console.log("testing");
+				orderTaker.attributes['src'].nodeValue = "pizza-man-left.gif";
+				grabPizza;
+			}
+			else {
+				orderTaker.attributes['src'].nodeValue = "pizza-man-right.gif";
+			}
+		  break;
+	}
+
+}
+orderTaker.addEventListener("animationstart",takeOrder, false);
+orderTaker.addEventListener("animationend",takeOrder, false);
+orderTaker.addEventListener("animationiteration",takeOrder, false);
+// cook.addEventListener("animationstart",grabPizza, false);
+// cook.addEventListener("animationend",grabPizza, false);
+// cook.addEventListener("animationiteration",grabPizza, false);
+
+pause.addEventListener("click", toggleChef, false);
